c9dffdd93901adfa1dce3e17c257b4f74e97b878
pebabion <pebabion@users.noreply.github.com>
2024-01-06 19:16:20 +0800

    chore: remove credential path from backend configuration

--- gcp/backend.tf
+++ gcp/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "pebabion-terraform-backend"
-    prefix      = "terraform/state"
-    credentials = "service_account.json"
+    bucket = "pebabion-terraform-backend"
+    prefix = "terraform/state"
   }
 }
