7a80f795474267059f265dbd546114111ec4dda0
pebabion <28013335+pebabion@users.noreply.github.com>
2024-11-11 15:05:45 -0500

    feat: add heroku

---
+++ heroku/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "heroku" {
+    # API Key is expected to be set via HEROKU_API_KEY environment variable
+}
+
